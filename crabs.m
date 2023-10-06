c1a2c5a55f9f52ae53f6e0cbf16a9e58119d6a98
DuckonDuck <wolf101x7@gmail.com>
2023-10-06 14:56:52 -0700

    I need help

--- crabs.m
+++ crabs.m
@@ -11,8 +11,24 @@ function crabs ()
   % Draw the captain and initialize graphics handles
   %*********************************************************
   % Put your call to drawCapt() here ..... You must give drawCapt its
-  drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+    captGraphics = drawCapt( xCapt, yCapt, thetaCapt, sizeCapt);
   % input and output arguments.
   %*******************************************************
-endfunction
+  cmd = "null"; % initial command
+  while ( cmd != "Q") % While not quit, read keyboard and respond
+  cmd = kbhit(); % Read the keyboard.
+  if( cmd == "w" || cmd == "a" || cmd == "d" ) %Captain has moved. Respond.
+  % erase old captain
 
+  for i=1:length( captGraphics )
+  set( captGraphics(i), 'Visible', 'off' );
+  endfor
+  % move capt
+  % [xCapt, yCapt, thetaCapt] = moveCapt(cmd, xCapt, yCapt, thetaCapt);
+  % draw new capt
+
+  endif
+
+  endwhile
+
+endfunction
